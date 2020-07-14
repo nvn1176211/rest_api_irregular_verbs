93ce5a80be146e2548c124784d69ecb938a54d9a
DN_NhanNV <nhannv@mcrew-tech.com>
2020-07-14 17:23:09 +0700

    done update data api

--- app/Http/Controllers/Api/VerbsController.php
+++ app/Http/Controllers/Api/VerbsController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Cache;
-
+use Database;
+use Illuminate\Support\Facades\Validator;
 
 class VerbsController extends Controller
 {
@@ -16,7 +17,51 @@ class VerbsController extends Controller
      */
     public function index()
     {
-        return response($this->irregular_verbs, 200);
+        // dd(Database::IRREGULAR_VERBS['choose']);
+        $v1 = 'choose';
+        if( !isset(Database::IRREGULAR_VERBS[$v1]['v2']) ){
+            die('V2 not found!');
+        }
+        if( !isset(Database::IRREGULAR_VERBS[$v1]['v3']) ){
+            die('V3 not found!');
+        }
+        $group_text_verb = '"choose"=>["v2"=>"'.Database::IRREGULAR_VERBS[$v1]['v2'].'","v3"=>"'.Database::IRREGULAR_VERBS[$v1]['v3'].'"],';
+        dd($group_text_verb);
+        
+        $new_v2 = 'chosewww';
+        $v1 = 'choose';
+        $patten = '|\"'.$v1.'\"=>\[\"v2\"=>\"(.*)\",\"v3\"=>\"(.*)\"\]|U';
+        // dd($patten);
+        preg_match_all(
+            $patten,
+            file_get_contents('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php'),
+            $out1,
+            PREG_PATTERN_ORDER
+        );
+        if(empty($out1[0][0])){
+            die("V1 isn't exist. Please check again!");
+        }
+        $fullItem = $out1[0];
+        dd($out1);
+
+        // readfile('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php');
+        
+        // $myfile = fopen('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php', "r") or die("Unable to open file!");
+        // echo fread($myfile,filesize('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php'));
+        // fclose($myfile);
+
+        // die("Unable to open file!");
+        // dd(readfile('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php'));
+        // preg_match_all(
+        //     "|const IRREGULAR_VERBS = (.*);|U",
+        //     readfile('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php'),
+        //     $out1,
+        //     PREG_PATTERN_ORDER
+        // );
+        // dd($out1);
+        
+        // dd(Database::IRREGULAR_VERBS);
+        // return response(Database::IRREGULAR_VERBS, 200);
     }
 
     /**
@@ -27,7 +72,94 @@ class VerbsController extends Controller
      */
     public function store(Request $request)
     {
-        return response(isset($request['v2']), 200);
+        $result = [];
+        $validator = Validator::make($request->all(), [
+            'v1' => 'required|alpha',
+            'v2' => 'alpha',
+            'v3' => 'alpha',
+        ]);
+        if($validator->fails()){
+            $result['error'] = [
+                'message' => 'Validate is false. Please check again!',
+                'errors' => $validator->errors(),
+            ];
+            return response($result, 400);
+        }
+
+        if( !isset($request['v2']) && !isset($request['v3']) ){
+            return response('There must be at least one in v2 and v3', 400);   
+        }
+        if( isset($request['v2']) ){
+            $new_v2 = $request['v2'];
+        }
+        if( isset($request['v3']) ){
+            $new_v3 = $request['v3'];
+        }
+
+        $v1 = $request['v1'];
+        if(empty(Database::IRREGULAR_VERBS[$v1])){
+            return response('V1 not found. Please pass a valid v1', 400);   
+        }
+
+        if( !isset(Database::IRREGULAR_VERBS[$v1]['v2']) || !isset(Database::IRREGULAR_VERBS[$v1]['v3']) ){
+            $result['error'] = [
+                'message' => 'Wrong database. Please contact the admin!',
+                'errors' => 'Database is wrong at '.$v1,
+            ];
+            return response($result, 500);
+        }
+
+        $v2 = Database::IRREGULAR_VERBS[$v1]['v2'];
+        $v3 = Database::IRREGULAR_VERBS[$v1]['v3'];
+        $group_text_verb = '"'.$v1.'"=>["v2"=>"'.$v2.'","v3"=>"'.$v3.'"],';
+        $new_group_text_verb = '"'.$v1.'"=>["v2"=>"'.($new_v2 ?? $v2).'","v3"=>"'.($new_v3 ?? $v3).'"],';
+
+        $old_file_content = file_get_contents('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php');
+        $new_file_content = str_replace($group_text_verb, $new_group_text_verb, $old_file_content);
+
+        if(!file_exists('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php')){
+            $result['error'] = [
+                'message' => 'Wrong database. Please contact the admin!',
+                'errors' => 'Database file path is wrong',
+            ];
+            return response($result, 500);
+        }
+
+        $db_file = fopen('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php', "w") or die(json_encode([
+            'error' => [
+                'message' => 'Wrong database. Please contact the admin!',
+                'errors' => "Database file can't be opened",
+            ]
+        ]));
+        fwrite($db_file, $new_file_content);
+        fclose($db_file);
+        return response('ok', 200);   
+        
+        die(123);
+        // $a = readfile('C:\xampp\htdocs\rest_api_irregular_verbs\app\Database.php').'-hello';
+        // return response($a, 200);
+        
+        // $a = file_get_contents;
+        preg_match_all(
+            "|const(.*)awake|U",
+            $a,
+            $out1,
+            PREG_PATTERN_ORDER
+        );
+        return response($out1, 200);
+        return response($result, 200);
+        
+        // $result = [];
+        // $param1 = 2;
+        // $param2 = 2;
+        // $param3 = 2;
+        // if(isset($request['v2'])){
+        //     $result['v2'] = $request['v2'];
+        // }
+        // if(isset($request['v3'])){
+        //     $result['v3'] = $request['v3'];
+        // }
+        // return response(isset($request['v2']), 200);
     }
 
     /**
@@ -38,8 +170,8 @@ class VerbsController extends Controller
      */
     public function show($v1)
     {
-        if(!empty($this->irregular_verbs[$v1])){
-            $result[$v1] = $this->irregular_verbs[$v1];
+        if(!empty(Database::IRREGULAR_VERBS[$v1])){
+            $result[$v1] = Database::IRREGULAR_VERBS[$v1];
             return response($result, 200);   
         }else{
             return response('V1 not valid. Please pass a valid v1', 400);       
@@ -75,5 +207,5 @@ class VerbsController extends Controller
     // $out1 = curl_exec($ch);
     // curl_close($ch);
 
-    public $irregular_verbs = ["awake"=>["v2"=>"awoke","v3"=>"awoken"],"be"=>["v2"=>"was, were","v3"=>"been"],"beat"=>["v2"=>"beat","v3"=>"beaten"],"become"=>["v2"=>"became","v3"=>"become"],"begin"=>["v2"=>"began","v3"=>"begun"],"bend"=>["v2"=>"bent","v3"=>"bent"],"bet"=>["v2"=>"bet","v3"=>"bet"],"bid"=>["v2"=>"bid","v3"=>"bid"],"bite"=>["v2"=>"bit","v3"=>"bitten"],"blow"=>["v2"=>"blew","v3"=>"blown"],"break"=>["v2"=>"broke","v3"=>"broken"],"bring"=>["v2"=>"brought","v3"=>"brought"],"broadcast"=>["v2"=>"broadcast","v3"=>"broadcast"],"build"=>["v2"=>"built","v3"=>"built"],"burn"=>["v2"=>"burned or<\/em> burnt","v3"=>"burned or<\/em> burnt"],"buy"=>["v2"=>"bought","v3"=>"bought"],"catch"=>["v2"=>"caught","v3"=>"caught"],"choose"=>["v2"=>"chose","v3"=>"chosen"],"come"=>["v2"=>"came","v3"=>"come"],"cost"=>["v2"=>"cost","v3"=>"cost"],"cut"=>["v2"=>"cut","v3"=>"cut"],"dig"=>["v2"=>"dug","v3"=>"dug"],"do"=>["v2"=>"did","v3"=>"done"],"draw"=>["v2"=>"drew","v3"=>"drawn"],"dream"=>["v2"=>"dreamed or<\/em> dreamt","v3"=>"dreamed or<\/em> dreamt"],"drive"=>["v2"=>"drove","v3"=>"driven"],"drink"=>["v2"=>"drank","v3"=>"drunk"],"eat"=>["v2"=>"ate","v3"=>"eaten"],"fall"=>["v2"=>"fell","v3"=>"fallen"],"feel"=>["v2"=>"felt","v3"=>"felt"],"fight"=>["v2"=>"fought","v3"=>"fought"],"find"=>["v2"=>"found","v3"=>"found"],"fly"=>["v2"=>"flew","v3"=>"flown"],"forget"=>["v2"=>"forgot","v3"=>"forgotten"],"forgive"=>["v2"=>"forgave","v3"=>"forgiven"],"freeze"=>["v2"=>"froze","v3"=>"frozen"],"get"=>["v2"=>"got","v3"=>"got (sometimes<\/em> gotten)"],"give"=>["v2"=>"gave","v3"=>"given"],"go"=>["v2"=>"went","v3"=>"gone"],"grow"=>["v2"=>"grew","v3"=>"grown"],"hang"=>["v2"=>"hung","v3"=>"hung"],"have"=>["v2"=>"had","v3"=>"had"],"hear"=>["v2"=>"heard","v3"=>"heard"],"hide"=>["v2"=>"hid","v3"=>"hidden"],"hit"=>["v2"=>"hit","v3"=>"hit"],"hold"=>["v2"=>"held","v3"=>"held"],"hurt"=>["v2"=>"hurt","v3"=>"hurt"],"keep"=>["v2"=>"kept","v3"=>"kept"],"know"=>["v2"=>"knew","v3"=>"known"],"lay"=>["v2"=>"laid","v3"=>"laid"],"lead"=>["v2"=>"led","v3"=>"led"],"learn"=>["v2"=>"learned or<\/em> learnt","v3"=>"learned or<\/em> learnt"],"leave"=>["v2"=>"left","v3"=>"left"],"lend"=>["v2"=>"lent","v3"=>"lent"],"let"=>["v2"=>"let","v3"=>"let"],"lie"=>["v2"=>"lay","v3"=>"lain"],"lose"=>["v2"=>"lost","v3"=>"lost"],"make"=>["v2"=>"made","v3"=>"made"],"mean"=>["v2"=>"meant","v3"=>"meant"],"meet"=>["v2"=>"met","v3"=>"met"],"pay"=>["v2"=>"paid","v3"=>"paid"],"put"=>["v2"=>"put","v3"=>"put"],"read"=>["v2"=>"read","v3"=>"read"],"ride"=>["v2"=>"rode","v3"=>"ridden"],"ring"=>["v2"=>"rang","v3"=>"rung"],"rise"=>["v2"=>"rose","v3"=>"risen"],"run"=>["v2"=>"ran","v3"=>"run"],"say"=>["v2"=>"said","v3"=>"said"],"see"=>["v2"=>"saw","v3"=>"seen"],"sell"=>["v2"=>"sold","v3"=>"sold"],"send"=>["v2"=>"sent","v3"=>"sent"],"show"=>["v2"=>"showed","v3"=>"showed or<\/em> shown"],"shut"=>["v2"=>"shut","v3"=>"shut"],"sing"=>["v2"=>"sang","v3"=>"sung"],"sink"=>["v2"=>"sank","v3"=>"sunk"],"sit"=>["v2"=>"sat","v3"=>"sat"],"sleep"=>["v2"=>"slept","v3"=>"slept"],"speak"=>["v2"=>"spoke","v3"=>"spoken"],"spend"=>["v2"=>"spent","v3"=>"spent"],"stand"=>["v2"=>"stood","v3"=>"stood"],"stink"=>["v2"=>"stank","v3"=>"stunk"],"swim"=>["v2"=>"swam","v3"=>"swum"],"take"=>["v2"=>"took","v3"=>"taken"],"teach"=>["v2"=>"taught","v3"=>"taught"],"tear"=>["v2"=>"tore","v3"=>"torn"],"tell"=>["v2"=>"told","v3"=>"told"],"think"=>["v2"=>"thought","v3"=>"thought"],"throw"=>["v2"=>"threw","v3"=>"thrown"],"understand"=>["v2"=>"understood","v3"=>"understood"],"wake"=>["v2"=>"woke","v3"=>"woken"],"wear"=>["v2"=>"wore","v3"=>"worn"],"win"=>["v2"=>"won","v3"=>"won"],"write"=>["v2"=>"wrote","v3"=>"written"]];
+   
 }
